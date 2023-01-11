2fdb35e5b201d446c53fa5e6cec61fefb16ae867
ankush-vapta <vapta.ankush@gmail.com>
2023-01-11 10:25:59 +0530

    4th commit

--- src/ReduxToolkit/Components/ToolkitProducts.jsx
+++ src/ReduxToolkit/Components/ToolkitProducts.jsx
@@ -1,21 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { height } from '@mui/system';
 
 const ToolkitProducts = () => {
     const [Products, setProducts] = useState([]);
@@ -36,6 +20,7 @@ const ToolkitProducts = () => {
                     {Products.map((item) =>
                         <div className="col-md-3  my-3" key={item.id}>
                             <div className="card" >
+
                                 <img className="card-img-top" src={item.image} alt="Card image cap" style={{ "height": "400px" }} />
                                 <div className="card-body">
                                     <h5 className="card-title">{item.category}</h5>
